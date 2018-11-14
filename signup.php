172e976e9fd1b2db4d66709bade2b6066e6c40d3
Ajay Police Patil <37379256+ajay1297@users.noreply.github.com>
2018-11-14 18:39:34 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -7,12 +7,16 @@
         $username = $_POST['username']; 
         $email = $_POST['email']; 
         $password = $_POST['password'];
-        $sql = mysqli_query($con,"SELECT * FROM members WHERE username = '$_POST[username]' ") or die(mysqli_error($con));
-        $row = mysqli_fetch_array($sql,MYSQLI_ASSOC) or die(mysqli_error($con)); 
-        if($username == $row["username"])
-        { 
-            echo " Already registered";
-            exit();
+        //echo "".$fullname.$username.$email.$password."";
+        $sql = mysqli_query($con,"SELECT * FROM members WHERE username = '$_POST[username]'") or die(mysqli_error($con));
+        if(mysqli_num_rows($sql) > 0)
+        {
+            $row = mysqli_fetch_array($sql,MYSQLI_ASSOC) or die(mysqli_error($con)); 
+            if($username == $row["username"] || $password == $row["password"])
+            { 
+                echo " Try with other username/email";
+                exit();
+            }
         } 
         $query = "INSERT INTO members (fullname,username,email,password) VALUES ('$fullname','$username','$email','$password')"; 
         $data = mysqli_query ($con,$query)or die(mysqli_error($con)); 
