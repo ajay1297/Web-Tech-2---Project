0707ee6d4dc784d6033dc762213f50638252399c
Ajay Police Patil <37379256+ajay1297@users.noreply.github.com>
2018-11-14 18:44:00 +0530

    Update login.php

--- login.php
+++ login.php
@@ -5,17 +5,23 @@ if(isset($_POST['username'])){
     
    $username=$_POST['username'];
    $password=$_POST['password'];
-    
+    //echo $username;
+    //echo $password;
    $sql="SELECT * FROM members WHERE username = '".$username."' AND password = '".$password."' limit 1";
     
-   $result=mysqli_query($con , $sql) or die(mysqli_error($con));
-   $row = mysqli_fetch_array($result,MYSQLI_ASSOC) or die(mysqli_error($con)); 
-
-   if($row["username"] == $username && $row["password"] == $password)
+   $result=mysqli_query($con , $sql);
+   //echo $result;
+   if(mysqli_num_rows($result)>0)
    {
-     echo " You Have Successfully Logged in";
-     exit();
-   }
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC) or die(mysqli_error($con));
+      //echo $row["username"];
+      //echo $row["password"];
+      if($row["username"] == $username && $row["password"] == $password)
+      {
+         echo " You Have Successfully Logged in";
+         exit();
+      }
+   } 
    else
    {
       echo " You Have Entered Incorrect Password";
